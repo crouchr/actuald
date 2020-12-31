ace721a2e06b993641a394ddc09a3a80a4704817
crouchr <richard.crouch@vonage.com>
2020-12-31 16:57:12 +0000

    Added forecasts table

---
+++ ansible/dev/forecasts_table.sql
@@ -0,0 +1,16 @@
+USE metminidb;
+CREATE TABLE forecasts
+(
+id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ts_local DATETIME NOT NULL,
+ts_utc DATETIME NOT NULL,
+julian INT NOT NULL,
+location VARCHAR(64) NOT NULL,
+pressure INT NOT NULL,
+ptrend VARCHAR(10) NOT NULL,
+wind_quadrant VARCHAR(8) NOT NULL,
+wind_strength INT NOT NULL,
+slope FLOAT NOT NULL,
+source VARCHAR(32) NOT NULL,
+forecast VARCHAR(128) NOT NULL
+);
