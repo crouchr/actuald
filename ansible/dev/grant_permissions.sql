e7d272fe1d27c954c6825772e176ec491cfa3d04
crouchr <richard.crouch@vonage.com>
2021-01-01 09:55:55 +0000

    Simplified permissions

--- ansible/dev/grant_permissions.sql
+++ ansible/dev/grant_permissions.sql
@@ -1,6 +1,13 @@
-CREATE USER 'metmini'@metmini.localdomain identified by 'metmini';
+CREATE USER 'metmini' identified by 'metmini';
 CREATE USER 'grafanaReader' IDENTIFIED BY 'grafanasecret';
 GRANT SELECT ON metminidb.actual TO 'grafanaReader';
 GRANT ALL ON metminidb.* to 'metmini' identified by 'metmini';
 FLUSH PRIVILEGES;
-SHOW GRANTS FOR 'metmini'@metmini.localdomain;
+-- SHOW GRANTS FOR 'metmini';
+
+-- CREATE USER 'metmini'@metmini.localdomain identified by 'metmini';
+-- CREATE USER 'grafanaReader' IDENTIFIED BY 'grafanasecret';
+-- GRANT SELECT ON metminidb.actual TO 'grafanaReader';
+-- GRANT ALL ON metminidb.* to 'metmini' identified by 'metmini';
+-- FLUSH PRIVILEGES;
+-- SHOW GRANTS FOR 'metmini'@metmini.localdomain;
